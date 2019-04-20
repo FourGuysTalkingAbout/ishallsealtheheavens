c8660e846e5d2c8f7c23620afd2bd1ef11392dc9
tviloria <terrenceviloria93@gmail.com>
2019-04-19 22:47:47 -0700

    FS-48: properly named class names

--- lib/instance.dart
+++ lib/instance.dart
@@ -3,7 +3,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:ishallsealtheheavens/app_bar_top.dart';
-
 import 'app_bar_bottom.dart';
 import 'join_create.dart';
 
