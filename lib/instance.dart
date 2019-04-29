0022022239f285abca8141df5962cc66face8ebb
lestgabo <lestgabo@gmail.com>
2019-04-28 21:32:19 -0700

    FS-3 add date into image names

--- lib/instance.dart
+++ lib/instance.dart
@@ -7,8 +7,10 @@ import 'package:image_picker/image_picker.dart';
 import 'app_bar_top_instance.dart';
 import 'app_bar_bottom.dart';
 
+import 'package:intl/intl.dart';
 
 final db = Firestore.instance;
+
 class InstancePage extends StatefulWidget {
   final String value; //TODO: is this needed?
 
@@ -19,18 +21,19 @@ class InstancePage extends StatefulWidget {
 }
 
 class _InstancePageState extends State<InstancePage> {
-
-
   openCamera() async {
     //TODO: implement a better naming convention for the 'imageName'
-    final String imageName = '${Random().nextInt(100)}';
+//    final String imageName = '${Random().nextInt(100)}';
+    final now = DateTime.now().toLocal();
+    final formatter = DateFormat.MMMMEEEEd().add_Hm();
+    final date = formatter.format(now);
 
     File imageFile = await ImagePicker.pickImage(
         source: ImageSource.camera); //returns a File after picture is taken
 
     //'images' is a folder in Firebase Storage,
     final StorageReference storageRef =
-        FirebaseStorage.instance.ref().child('images').child('$imageName');
+        FirebaseStorage.instance.ref().child('images').child('$date');
 
     final StorageUploadTask uploadTask =
         storageRef.putFile(imageFile); // uploads file into Firebase Storage
@@ -41,11 +44,12 @@ class _InstancePageState extends State<InstancePage> {
     var url = await taskSnapshot.ref
         .getDownloadURL(); // takes the URL of the imageFile
 
-    DocumentReference DocRef = await db //uploads the URL into the collection photos
-        .collection('instances')
-        .document('instance1')
-        .collection('photos')
-        .add({'url': url});
+    DocumentReference DocRef =
+        await db //uploads the URL into the collection photos
+            .collection('instances')
+            .document('instance1')
+            .collection('photos')
+            .add({'url': url});
   }
 
   @override
@@ -53,12 +57,11 @@ class _InstancePageState extends State<InstancePage> {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       backgroundColor: Colors.white,
-      appBar: InstanceTopAppBar() ,
+      appBar: InstanceTopAppBar(),
       endDrawer: DrawerMenu(),
-      body: Center(
-        child: PhotoGridView()
+      body: Center(child: PhotoGridView()
 //        InstanceSecondAppBar()
-      ),
+          ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 15.0),
         child: IconButton(
@@ -80,31 +83,35 @@ class PhotoGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: db.collection('instances').document('instance1').collection('photos').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        snapshot.hasData ? CircularProgressIndicator() : print(snapshot.hasError);
-        return GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(0.0),// padding of the cards
-          childAspectRatio: 6.5/9.0, // size of the card
-          children:
-          snapshot.data.documents.map((DocumentSnapshot document){
-            return Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0), //Image.network padding
-                    child: Image.network(document['url']),
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        );
-      }
-    );
+        stream: db
+            .collection('instances')
+            .document('instance1')
+            .collection('photos')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          snapshot.hasData
+              ? CircularProgressIndicator()
+              : print(snapshot.hasError);
+          return GridView.count(
+            crossAxisCount: 2,
+            padding: EdgeInsets.all(0.0), // padding of the cards
+            childAspectRatio: 6.5 / 9.0, // size of the card
+            children: snapshot.data.documents.map((DocumentSnapshot document) {
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          const EdgeInsets.all(10.0), //Image.network padding
+                      child: Image.network(document['url']),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          );
+        });
   }
 }
-
