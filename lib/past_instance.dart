b1d83eb6cd58660bd74b423a0e855ae70db35503
aldbas <bascugaldwin@gmail.com>
2019-05-21 21:18:49 -0700

    FS-65 past_instance page app bar

--- lib/past_instance.dart
+++ lib/past_instance.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import  'app_bar_past_instance.dart';
 import 'app_bar_bottom.dart';
 
 class PastInstance extends StatefulWidget {
@@ -8,10 +8,24 @@ class PastInstance extends StatefulWidget {
 }
 
 class _PastInstanceState extends State<PastInstance> {
+  List<String> instanceNames = <String> ['A','B','C'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: InstanceTopAppBar(),
+      backgroundColor: Color(0xFFE5E5E5),
+      endDrawer: DrawerMenu(),
       bottomNavigationBar: CustomAppBar(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ListView.separated(itemBuilder: (BuildContext context, int index){
+
+            }, separatorBuilder: null, itemCount: null)
+          ],
+        ),
+      ),
 
     );
   }
