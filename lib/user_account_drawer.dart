2c8123a6dd67489dc06b103724e76a038d028efb
Octavio Villena <octaviovillena@hotmail.com>
2019-06-22 23:39:28 -0700

    FS-61 sign out button left side drawer

--- lib/user_account_drawer.dart
+++ lib/user_account_drawer.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
-
+import 'logic/login_authentication.dart';
 
 class UserAccountDrawer extends StatefulWidget {
-  UserAccountDrawer({Key key,}) : super(key: key);
+  UserAccountDrawer({
+    Key key,
+  }) : super(key: key);
+
   @override
   _UserAccountDrawerState createState() => _UserAccountDrawerState();
 }
@@ -15,38 +17,87 @@ class _UserAccountDrawerState extends State<UserAccountDrawer> {
       child: ListView(
         children: <Widget>[
           //TODO:Made it look like the Figma Design, don't know if it's worth all the extra widgets.
-          DrawerHeader(margin: EdgeInsets.all(0.0),
+          DrawerHeader(
+            margin: EdgeInsets.all(0.0),
             padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-            decoration: BoxDecoration(border: Border(
-            bottom: Divider.createBorderSide(context,color: Colors.black,width: 2.0)
-          ),
-              color: Color(0xFFD1C4E9)),
-            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom: Divider.createBorderSide(context,
+                        color: Colors.black, width: 2.0)),
+                color: Color(0xFFD1C4E9)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                CircleAvatar(backgroundImage: AssetImage('images/apex_lestley.png'),maxRadius: 50,),
-                Text('Kikashi-Sensei@hotmail.com',style: TextStyle(
-                  fontWeight: FontWeight.bold
-                )),
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/apex_lestley.png'),
+                  maxRadius: 50,
+                ),
+                Text('Kikashi-Sensei@hotmail.com',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
               ],
             ),
           ),
-          Container(color: Color(0xff757575),
+          Container(
+            color: Color(0xff757575),
             height: 10.0,
           ),
+          ListTile(),
+          Divider(
+            color: Colors.black,
+          ),
           ListTile(
+            title: Text('Username:'),
           ),
-          Divider(color:Colors.black,),
-          ListTile(title: Text('Username:'),
+          Divider(
+            color: Colors.black,
           ),
-          Divider(color:Colors.black,),
+          ListTile(),
+          Divider(
+            color: Colors.black,
+          ),
+          ListTile(),
+          Divider(
+            color: Colors.black,
+          ),
           ListTile(
+            title: LoginButton(),
           ),
-          Divider(color:Colors.black,),
-          ListTile(
-          ),
-          Divider(color:Colors.black,),
+
+          Divider(
+            color: Colors.black,
+          )
         ],
       ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: authService.user,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return FlatButton(
+            onPressed: () => authService.signOut(),
+            textColor: Colors.black,
+            child: Container(
+              alignment: Alignment(-1.17, 0),
+              child: Text('Sign Out'),
+            ),
+          );
+        } else {
+          return FlatButton(
+            onPressed: () => authService.googleSignIn(),
+            textColor: Colors.black,
+            child: Container(
+              alignment: Alignment(-1.17, 0),
+              child: Text('Sign In'),
+            ),
+          );
+        }
+      },
     );
   }
 }
