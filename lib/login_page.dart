f328afb51bd3c077c91663f6f23afb7d9086ab0a
aldbas <bascugaldwin@gmail.com>
2019-08-05 21:19:30 -0700

    FS-69 facebook login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'logic/login_authentication.dart';
+import 'logic/fb_login.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isFBLoggedIn = false;
+  bool triedCheckLoggedIn = false;
+
+
+  void onFBLoginStatusChanged(bool isLoggedIn) {
+    setState(() {
+      this.isFBLoggedIn = isLoggedIn;
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+    if(triedCheckLoggedIn == false) {
+      _checkLoggedIn();
+    }
+
     return Scaffold(
       backgroundColor: const Color(0xff673AB7),
       body: Center(
@@ -19,6 +41,17 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
+
+  Future _checkLoggedIn() async {
+    GoogleSignInAccount googleuser = GoogleSignIn().currentUser;
+    if (googleuser == null) {
+    await authService.googleSignIn();
+
+    }else if (FaceBookLogin().isFBLoggedin == false){
+      await authService.initiateFacebookLogin();
+    }else
+      return AfterLogIn();
+  }
 }
 
 class UserProfile extends StatefulWidget {
@@ -29,6 +62,10 @@ class UserProfile extends StatefulWidget {
 class _UserProfileState extends State<UserProfile> {
   Map<String, dynamic> _profile;
   bool _loading = false;
+
+
+
+
 
   @override // if you setup listeners on a stream or an observable need to initialize the state
   initState() {
@@ -53,6 +90,30 @@ class _UserProfileState extends State<UserProfile> {
     );
   }
 }
+
+
+class AfterLogIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListBody(
+      children: <Widget>[
+        MaterialButton(
+          onPressed: () => Navigator.of(context).pushNamed('JoinCreate'),
+          color: Colors.white,
+          textColor: Colors.black,
+          child: Text('Join or Create an Instance'),
+        ),
+        MaterialButton(
+          onPressed: () => authService.signOut(),
+          color: Colors.white,
+          textColor: Colors.black,
+          child: Text('Sign out'),
+        ),
+      ],
+    );
+  }
+}
+
 
 class LoginButton extends StatelessWidget {
   @override
@@ -87,7 +148,10 @@ class LoginButton extends StatelessWidget {
                 child: Text('Login with Email'),
               ),
               MaterialButton(
-                onPressed: () => {},
+                onPressed: () {
+                  authService.initiateFacebookLogin();
+
+                } ,
                 color: Color(0xff3b5998),
                 textColor: Colors.white,
                 child: Text('Login with Facebook'),
